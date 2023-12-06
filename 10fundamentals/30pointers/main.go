e5df55754194390713514e43b37f01aec524478c
Zhunusov.S <sairan1994@gmail.com>
2023-12-06 22:31:39 +0600

    add pointers info

---
+++ 10fundamentals/30pointers/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	in := []int{64, 8, 32, 16}
+
+	out := make([]*int, len(in))
+
+	// idx and value reused over all iterations.
+	for idx, value := range in {
+		value := value
+		out[idx] = &value
+	}
+
+	for idx, value := range out {
+		fmt.Printf(
+			"idx: %d, value %d, size of ix: %d, size of value: %d\n",
+			idx,
+			*value,
+			unsafe.Sizeof(idx),
+			unsafe.Sizeof(value),
+		)
+
+	}
+}
